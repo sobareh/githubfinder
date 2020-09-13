4156b63f6a1e39ed2b94dfa167775b6fa01a5118
Achmad Sobari <ahmadsobarry@gmail.com>
2020-09-13 23:12:15 +0700

    Implement Context, Create Reducer&Actions, move searchUsers to Context

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Search = ({ showClear, clearUsers, searchUsers, setAlert }) => {
+import GithubContext from '../../context/github/githubContext';
+
+const Search = ({ showClear, clearUsers, setAlert }) => {
+  const githubContext = useContext(GithubContext);
+
   const [text, setText] = useState('');
 
   const onChange = e => setText(e.target.value);
@@ -11,7 +15,7 @@ const Search = ({ showClear, clearUsers, searchUsers, setAlert }) => {
     if (text === '') {
       setAlert('Please fill the form with something...', 'light');
     } else {
-      searchUsers(text);
+      githubContext.searchUsers(text);
       setText('');
     }
   };
@@ -42,7 +46,6 @@ const Search = ({ showClear, clearUsers, searchUsers, setAlert }) => {
 };
 
 Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
   clearUsers: PropTypes.func.isRequired,
   showClear: PropTypes.bool.isRequired,
   setAlert: PropTypes.func.isRequired,
